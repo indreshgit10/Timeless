8abeafcfa88aa65cbae3737e4f3d174bb2b66780
indreshgit10 <indreshkumar456789@gmail.com>
2025-12-17 15:21:50 +0530

    implemented cloudinary and connected mongodb

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+connectCloudinary();
+
+ //mw
+app.use(express.json());
+app.use(cors());
+
+
+//routes
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+app.listen(PORT, () => {console.log("server starts")});
